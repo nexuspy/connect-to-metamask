a15ba954cc3dda27b23c88a1a961da30926a27f2
Pratik <59784880+nexuspy@users.noreply.github.com>
2024-01-11 20:13:00 +0000

    wubaluba

--- helloreact/my-app/src/App.js
+++ helloreact/my-app/src/App.js
@@ -1,52 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
 import { useEffect, useState } from "react";
 import { Contract, providers } from "ethers";
 
 function App() {
-
   const [isWalletInstalled, setIsWalletInstalled] = useState(false);
-  // state for keeping track of current connected account.
   const [account, setAccount] = useState(null);
 
   useEffect(() => {
-        if (window.ethereum) {
-            setIsWalletInstalled(true);
-        }
-    }, []);
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
 
   async function connectWallet() {
-        window.ethereum
-            .request({
-                method: "eth_requestAccounts",
-            })
-            .then((accounts) => {
-                setAccount(accounts[0]);
-            })
-            .catch((error) => {
-                alert("Something went wrong");
-            });
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    } catch (error) {
+      alert("Something went wrong");
     }
-
-    if (account === null) {
-      return (
-        <div className="App">
-          {
-            isWalletInstalled ? (
-              <button onClick={connectWallet}>Connect Wallet</button>
-            ) : (
-              <p>Install Metamask wallet</p>
-            )
-          }
-
-        </div>
-      );
-    }
-      return (
-        <div className="App">
-          <p>Connected as: {account}</p>
-        </div>
-    );
   }
+
+  return (
+    <div className="App">
+      {
+        account === null ? (
+          <div>
+            {
+              isWalletInstalled ? (
+                <button className="btn" onClick={connectWallet}>
+                  Connect Wallet
+                </button>
+              ) : (
+                <p>Install MetaMask wallet</p>
+              )
+            }
+          </div>
+        ) : (
+          <div>
+            <p>Connected as: {account}</p>
+          </div>
+        )
+      }
+    </div>
+  );
+}
+
 export default App;
